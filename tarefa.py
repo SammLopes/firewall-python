60f5de5340de6a80abdc3f0da848bac49667e4c4
SammLopes <sam.v.p.lopes@gmail.com>
2024-10-13 17:01:39 -0300

    Adicionado tarefa e arquivo de teste de firewall

--- tarefa.py
+++ tarefa.py
@@ -6,58 +6,85 @@ import threading
 from scapy.all import sniff
 from datetime import datetime
 
-
 regras_firewall = {
     'allow': [],
-    'block': [{'ip_org': '192.168.0.1', 'ip_dst': 22}]
+    'block': [{'ip_org': '192.168.0.1', 'ip_dst': 22}],
+    'trainer_group':['192.168.0.10', '192.168.0.11']
 }
 
-pacotes_bloqueados = []
-
-trainers_group_ips = ['192.168.0.10', '192.168.0.11']
+pacotes_interceptados = []
 
 def verificarPacote(packet):
 
     print(packet);
-    #VErificar o pacote se ele é TCP oi IP
-    
-    
+    if regra_permitir_http(packet):
+        print("Pacote HTTP permitido")
+        
+        # Verificar se é um conteúdo de vídeo bloqueado
+        if verificar_conteudo_video(packet):
+            print("Conteúdo de vídeo permitido")
+          
+            return True  # Se não for bloqueado, o pacote é permitido
 
-
-def is_trainer(src_ip):
-    return src_ip in trainers_group_ips
-
-def is_night_time():
-    current_hour = datetime.now().hour
-    return current_hour >= 22 or current_hour < 6
+        print("Conteúdo de vídeo bloqueado")
+        return False  # Se for vídeo bloqueado, retorna False
+    else:
+        print("Pacote não permitido")
+        return False  # Se não for HTTP, retorna False
 
 def regra_permitir_http(packet):
     if packet.haslayer(IP) and packet.haslayer(TCP):
         src_ip = packet[IP].src
         dst_port = packet[TCP].dport
         
-        # Permitir somente se a porta for 80
         if dst_port == 80:
-            print(f"Pacote permitido: {src_ip} para porta {dst_port}")
+
+            pacotes_interceptados.append(f"Pacote permitido: {src_ip} para porta {dst_port}")
+            print('Lista de pacotes bloqueados ',pacotes_interceptados ,' \n');
             return True
         else:
-            pacotes_bloqueados.append(f"Bloqueado pacote {src_ip} para porta {dst_port}")
-            print(f"Bloqueado pacote {src_ip} para porta {dst_port}")
+            pacotes_interceptados.append(f"Bloqueado pacote {src_ip} para porta {dst_port}")
+            print(f"Bloqueado pacote {src_ip} para porta {dst_port} \n")
             return False
     else:
         print("Pacote não é IP/TCP")
-        pacotes_bloqueados.append(f"Pacote não processado: {packet.summary()}")
+        pacotes_interceptados.append(f"Pacote não processado: {packet.summary()}")
         return False
 
-   # Identificando trafego de video 
-        # if content_type and any(video_type in content_type.lower() for video_type in ['video/mp4', 'video/webm', 'video/ogg', 'video/mpeg']):
-        #     if is_trainer(src_ip):
-        #         if is_night_time:
-        #             pacotes_bloqueados.append(f'Pacote de vídeo permitido para Trainer: {src_ip}')
-        #             return True
-        #         else:
-        #             pacotes_bloqueados.append(f'Bloqueado pacote de vídeo para Trainer à noite: {src_ip}')
-        #             return False
-        #     else:
-        #         pacotes_bloqueados.append(f'Bloqueado pacote de vídeo: {src_ip}')
-        #         return False
+def verificar_conteudo_video(packet):
+    if packet.haslayer(HTTPResponse):
+        http_layer = packet[HTTPResponse]
+        content_type = http_layer.getfieldval('Content_Type')
+        src_ip = packet[IP].src
+        dst_port = packet[TCP].dport
+
+        if isinstance(content_type, bytes):
+            print('Teste content')
+            content_type = content_type.decode('utf-8')
+
+        if content_type and any(video_type in content_type.lower() for video_type in ['video/mp4', 'video/webm', 'video/ogg', 'video/mpeg']):
+            
+            if is_trainer(src_ip):      
+
+                if is_night_time():
+                    print('------- Pacote a noite -------')
+                    pacotes_interceptados.append(f"Bloqueado pacote de vídeo para Trainer à noite: {src_ip} para porta {dst_port}")
+                    print(f"Bloqueado pacote de vídeo para Trainer à noite: {src_ip} para porta {dst_port}")
+                    return False
+                else:
+                    pacotes_interceptados.append(f"Pacote de vídeo permitido para Trainer: {src_ip} para porta {dst_port}")
+                    print(f"Pacote de vídeo permitido para Trainer: {src_ip} para porta {dst_port}")
+                    return True
+            pacotes_interceptados.append(f"Bloqueado conteúdo de vídeo: {src_ip} para porta {dst_port}")
+            print(f"Bloqueado conteúdo de vídeo: {src_ip} para porta {dst_port} \n")
+            return False
+
+        return True
+          
+
+def is_trainer(src_ip):
+    return src_ip in regras_firewall['trainer_group']
+
+def is_night_time():
+    current_hour = datetime.now().hour
+    return current_hour >= 22 or current_hour < 6
--- teste_firewall.py
+++ teste_firewall.py
@@ -1,51 +1,53 @@
 import unittest
 from scapy.all import IP, TCP
 from scapy.layers.http import HTTPResponse
-from tarefa import verificarPacote, pacotes_bloqueados
+from tarefa import verificarPacote, pacotes_interceptados
 from unittest import mock
 from datetime import datetime
 
 class TesteFirewall(unittest.TestCase):
 
     def setUp(self):
-        # Limpa a lista de pacotes bloqueados antes de cada teste
-        global pacotes_bloqueados
-        pacotes_bloqueados = []
-
+        pacotes_interceptados.clear();
     def test_permit_http(self):
         # Simula um pacote HTTP
         packet = IP(src="192.168.0.5") / TCP(dport=80) /  HTTPResponse(Content_Type="text/html")
         self.assertTrue(verificarPacote(packet))
-        self.assertIn('Pacote permitido: 192.168.0.5 para porta 80', pacotes_bloqueados)
+        print('Lista de pacote no test ',pacotes_interceptados);
+        self.assertIn('Pacote permitido: 192.168.0.5 para porta 80', pacotes_interceptados)
+       
+    def test_block_non_http(self):
+        # Simula um pacote em uma porta não HTTP
+        packet = IP(src="192.168.0.5") / TCP(dport=22)
+        self.assertFalse(verificarPacote(packet))
+        print('Lista de pacote no test ',pacotes_interceptados);
+        self.assertIn('Bloqueado pacote 192.168.0.5 para porta 22', pacotes_interceptados)
+       
+    def test_block_video_non_trainer(self):
+        # Simula um pacote de vídeo de um usuário não Trainer
+        packet = IP(src="192.168.0.6") / TCP(dport=80) / HTTPResponse(Content_Type='video/mp4')
+        self.assertFalse(verificarPacote(packet))
+        print('Lista de pacote no test ',pacotes_interceptados);
+        self.assertIn('Bloqueado conteúdo de vídeo: 192.168.0.6 para porta 80', pacotes_interceptados)
 
-    # def test_block_non_http(self):
-    #     # Simula um pacote em uma porta não HTTP
-    #     packet = IP(src="192.168.0.5") / TCP(dport=22)
-    #     self.assertFalse(verificarPacote(packet))
-    #     self.assertIn('Bloqueado pacote 192.168.0.5 para porta 22', pacotes_bloqueados)
+    def test_permit_video_trainer_day(self):
+        # Simula um pacote de vídeo de um Trainer durante o dia
+        print("Teste video trainers dia");
+        packet = IP(src="192.168.0.10") / TCP(dport=80) / HTTPResponse(Content_Type='video/mp4')
+        self.assertTrue(verificarPacote(packet))
+        self.assertIn('Pacote de vídeo permitido para Trainer: 192.168.0.10 para porta 80', pacotes_interceptados)
 
-    # def test_block_video_non_trainer(self):
-    #     # Simula um pacote de vídeo de um usuário não Trainer
-    #     packet = IP(src="192.168.0.6") / TCP(dport=80) / HTTPResponse(Content_Type='video/mp4')
-    #     self.assertFalse(verificarPacote(packet))
-    #     self.assertIn('Bloqueado pacote de vídeo: 192.168.0.6', pacotes_bloqueados)
-
-    # def test_permit_video_trainer_day(self):
-    #     # Simula um pacote de vídeo de um Trainer durante o dia
-    #     packet = IP(src="192.168.0.10") / TCP(dport=80) / HTTPResponse(Content_Type='video/mp4')
-    #     self.assertTrue(verificarPacote(packet))
-    #     self.assertIn('Pacote de vídeo permitido para Trainer: 192.168.0.10', pacotes_bloqueados)
-
-    # def test_block_video_trainer_night(self):
-    #     # Simula um pacote de vídeo de um Trainer à noite
-    #     packet = IP(src="192.168.0.10") / TCP(dport=80) / HTTPResponse(Content_Type='video/mp4')
+    def test_block_video_trainer_night(self):
+        # Simula um pacote de vídeo de um Trainer à noite
+        packet = IP(src="192.168.0.10") / TCP(dport=80) / HTTPResponse(Content_Type='video/mp4')
         
-    #     # Força a hora atual para ser 23:00 (noite)
-    #     with mock.patch('tarefa.datetime') as mock_datetime:
-    #         mock_datetime.now.return_value = datetime(2024, 10, 12, 23, 0)
-    #         self.assertFalse(verificarPacote(packet))
-    #         self.assertIn('Bloqueado pacote de vídeo para Trainer à noite: 192.168.0.10', pacotes_bloqueados)
-
+        # Força a hora atual para ser 23:00 (noite)
+        with mock.patch('tarefa.datetime') as mock_datetime:
+            mock_datetime.now.return_value = datetime(2024, 10, 12, 23, 0)
+            self.assertFalse(verificarPacote(packet))
+            self.assertIn('Bloqueado pacote de vídeo para Trainer à noite: 192.168.0.10 para porta 80', pacotes_interceptados)
+    
 if __name__ == '__main__':
     unittest.main()
-    
+
+
